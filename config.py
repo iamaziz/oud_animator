7c82a500d430b9c8dd4e7848a6892b23794761f3
iamaziz <iamaziz.alto@gmail.com>
2020-04-03 00:16:26 -0400

    clean

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 """This file contains:
-1) the parameters for the Oud's neck segmentation and 
-2) the mapping of the notes to indices on the Master String
+1) the parameters for the Oud's neck (Zend) segmentation and
+2) the mapping of the notes to indices on the Oud's strings
 
 
 # -- help
@@ -20,42 +20,7 @@ STRING_LEN = int(MASTER_STRING_LEN / NUM_STRINGS)
 
 # MAP legend
 # ----------
-# NOTENAME: (StringNumber) + (Number of shift on the string)
-
-NOTES_MAP = {
-    # string #1
-    "FA": (STRING_LEN * 0),
-    "SOL": (STRING_LEN * 0) + (NOTES_INTERVAL * 2),
-    # string #2
-    "LA": (STRING_LEN * 1),
-    "SI": (STRING_LEN * 1) + (NOTES_INTERVAL * 1),
-    "DO": (STRING_LEN * 1) + (NOTES_INTERVAL * 3),
-    "REß": (STRING_LEN * 1) + (NOTES_INTERVAL * 4),
-    # string #3
-    "RE": (STRING_LEN * 2),
-    "MIß": (STRING_LEN * 2) + (NOTES_INTERVAL * 1),
-    "MI": (STRING_LEN * 2) + (NOTES_INTERVAL * 2),
-    "Fa": (STRING_LEN * 2) + (NOTES_INTERVAL * 3),
-    "Fa#": (STRING_LEN * 2) + (NOTES_INTERVAL * 4),
-    # string #4
-    "Sol": (STRING_LEN * 3),
-    "Laß": (STRING_LEN * 3) + (NOTES_INTERVAL * 1),
-    "La": (STRING_LEN * 3) + (NOTES_INTERVAL * 2),
-    "Si": (STRING_LEN * 3) + (NOTES_INTERVAL * 3),
-    "Si#": (STRING_LEN * 3) + (NOTES_INTERVAL * 4),
-    # string #5
-    "Do": (STRING_LEN * 4),
-    "Reß": (STRING_LEN * 4) + (NOTES_INTERVAL * 1),
-    "Re": (STRING_LEN * 4) + (NOTES_INTERVAL * 2),
-    "Mi": (STRING_LEN * 4) + (NOTES_INTERVAL * 3),
-    "Faß": (STRING_LEN * 4) + (NOTES_INTERVAL * 4),
-    # string #6
-    "fa": (STRING_LEN * 5),
-    "sol": (STRING_LEN * 5) + (NOTES_INTERVAL * 2),
-    "laß": (STRING_LEN * 5) + (NOTES_INTERVAL * 3),
-    "la": (STRING_LEN * 5) + (NOTES_INTERVAL * 4),
-}
-
+# NOTENAME: [StringNumber, FingerNumber "or number of interval shifts on the string"]
 
 NOTES_INDEX = {
     # string #1
