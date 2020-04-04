cf220792f6d0ff64cfcfb29507bed0c3d2bc3a04
iamaziz <iamaziz.alto@gmail.com>
2020-04-03 20:25:41 -0400

    add Khaieef Aqool of Mohammed Abdulwahab

---
+++ note_sheets/songs.py
@@ -0,0 +1,13 @@
+# -- Khaieef Aaqool
+# -- https://youtu.be/ns-70pRGbdc?t=138
+# Maqam Kurd: DO REß MIß Fa Sol Laß Siß Do Do Siß Laß Sol Fa MIß REß DO
+# Soluvage:
+khaieef = """
+Do  Sol Sol Laß   Siß    Sol  Laß Siß Sol Laß Fa
+Sol Fa MIß Fa Sol MIß RE MIß Fa Sol Sol
+
+Sol Fa MIß Fa Sol MIß RE MIß Fa Sol Sol
+
+Do Do Siß Do Reß Do Siß Siß Do  Siß Laß Laß Siß Laß Sol
+
+Fa Sol Laß Sol Fa MIß Fa Sol Fa MIß REß Fa Fa MIß MIß REß REß DO DO"""
